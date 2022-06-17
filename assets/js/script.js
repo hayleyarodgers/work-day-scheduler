5b69ca5920bf0545b2009386de8589d0f6781b95
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-06-17 14:13:38 +1000

    First attempt at setting timeblock colour based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,31 @@
     5. =
 */
 
+// 
+var currentTime = moment();
 
+// Display current date at top of calendar
 var currentDayEl = $('#currentDay');
-var currentDay = moment().format("dddd, MMMM Do");
+var currentDay = currentTime.format('dddd, MMMM Do');
 currentDayEl.text(currentDay);
 
-// 
+// Set colour of timeblock based on current time
+
+var descriptionEl = $('#description');
+
+var currentHour = currentTime.format('H');
+var rowHour = descriptionEl.data('hour');
+
+console.log(currentHour);
+console.log(rowHour);
+
+if (rowHour < currentHour) {
+    descriptionEl.addClass('past');
+} else if (rowHour = currentHour) {
+    descriptionEl.addClass('present');
+} else {
+    descriptionEl.addClass('future');
+};
+
+
+// Add event to local storage
