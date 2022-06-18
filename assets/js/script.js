910523f87086df96d2f98762105632bc7c20be2a
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-06-18 11:49:22 +1000

    Consolidate and comment on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,89 @@
 /* JS DIRECTORY
-    1. =
-    2. =
-    3. =
-    4. =
-    5. =
+    1. =VARIABLES
+    2. =APPEARANCE
+    3. =STORING-EVENTS
 */
 
+/* ===VARIABLES=== */
 
+// Section element variables
+var currentDayEl = $('#currentDay');
+var messageEl = $('#message');
+var descriptionEls = $('.description');
+var saveButtons = $('.fas');
+
+// Time-related variables
 var currentTime = moment();
+var currentDay = currentTime.format('dddd, MMMM Do');
 var currentHour = parseInt(currentTime.format('H'));
 
-// Display current date at top of calendar
-var currentDayEl = $('#currentDay');
-var currentDay = currentTime.format('dddd, MMMM Do');
+
+/* ===APPEARANCE=== */
+
+// Display current day at top of calendar
 currentDayEl.text(currentDay);
 
-// Set colour of timeblock based on current time
-var descriptionEls = $('.description');
-
+// Display coloured time-blocks with any saved events
 for (var i = 0; i < descriptionEls.length; i++) {
     var descriptionEl = descriptionEls[i];
 
+    setColour();
+    showSavedEvents();
+}
+
+// Set colour of each timeblock based on current time
+function setColour() {
     var rowHour = parseInt(descriptionEl.dataset.hour);
 
     if (rowHour < currentHour) {
-        descriptionEl.classList.add("past");
+        descriptionEl.classList.add('past');
     } else if (rowHour === currentHour) {
-        descriptionEl.classList.add("present");
+        descriptionEl.classList.add('present');
     } else if (rowHour > currentHour) {
-        descriptionEl.classList.add("future");
+        descriptionEl.classList.add('future');
     };
 }
+
+// Display any previously saved events
+function showSavedEvents() {
+    var descriptionID = descriptionEl.id;
+    var savedDescription = JSON.parse(localStorage.getItem(descriptionID));
+
+    if (savedDescription !== null) {
+        descriptionEl.textContent = savedDescription;
+    }
+}
+
+
+/* ===STORING-EVENTS=== */
 
 // Add event to local storage when save button clicked
 function saveEvent(event) {
     var buttonClicked = $(event.target);
-
     var selectedTimeBlock = buttonClicked.closest('.time-block');
     var selectedTimeblockTextarea = selectedTimeBlock.children('textarea');
-
-    var eventID = selectedTimeblockTextarea.attr("id");
+    var eventID = selectedTimeblockTextarea.attr('id');
     var eventDescription = selectedTimeblockTextarea.val();
 
     localStorage.setItem(eventID, JSON.stringify(eventDescription));
-    
-    var messageEl = $('#message');
-    messageEl.text('Appointment added to local storage ✓');
+
+    showMessage();
 }
 
-var saveButtons = $('.fas');
 saveButtons.on('click', saveEvent);
 
-// When page loads, check local storage for events and print on page
-function showSavedEvents() {
-    for (var i = 0; i < descriptionEls.length; i++) {
-        var descriptionEl = descriptionEls[i];
-        var descriptionID = descriptionEl.id;
-        var savedDescription = JSON.parse(localStorage.getItem(descriptionID));
+// Show five-second long message to say event has been saved
+function showMessage() {
+    messageEl.text('Appointment added to local storage ✓');
 
-        if (savedDescription !== null) {
-            descriptionEl.textContent = savedDescription;
-        }
-    }
+    setTimeout(function() {
+        messageEl.text('');
+    }, 5000);
 }
-
-window.onload = showSavedEvents();
 
 // Clear local storage between end of one day and start of another
 function clearLocalStorage() {
-    if (currentHour >= 18 || currentHour < 9) {
+    if (currentHour >= 18 || currentHour < 7) {
         localStorage.clear();
     }
 }
